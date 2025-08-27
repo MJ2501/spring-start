1947e391e595e4b1698d8c981a46f51f37e0cc61
mohit <mohit@gscpl.com>
2025-08-27 15:04:36 +0530

    init commit

---
+++ src/main/java/com/example/demo/controller/PersonController.java
@@ -0,0 +1,50 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Person;
+import com.example.demo.service.PersonService;
+
+@RestController
+@RequestMapping("/api/persons")
+public class PersonController {
+	
+	@Autowired
+	private PersonService personService;
+	
+	@GetMapping
+	public List<Person> getAllPersons() {
+		return personService.getAllPersons();
+	}
+	
+	@GetMapping("/{id}")
+	public Person getPersonById(@PathVariable Long id) {
+		return personService.getPersonById(id).orElseThrow();
+	}
+	
+	@PostMapping
+	public Person createPerson(@RequestBody Person person) {
+		return personService.savePerson(person);
+	}
+	
+	@PutMapping("/{id}")
+	public Person updatePerson(@PathVariable Long id, @RequestBody Person person) {
+		return personService.updatePerson(id, person);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deletePerson(@PathVariable Long id) {
+		personService.deletePerson(id);
+	}
+		
+}
---
+++ src/main/java/com/example/demo/service/PersonService.java
@@ -0,0 +1,39 @@
+package com.example.demo.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Person;
+import com.example.demo.repository.PersonRepository;
+
+@Service
+public class PersonService {
+	@Autowired
+	private PersonRepository personRepo;
+	
+	public List<Person> getAllPersons(){
+		return personRepo.findAll();
+	}
+	
+	public Optional<Person> getPersonById(Long id){
+		return personRepo.findById(id);
+	}
+	
+	public Person savePerson(Person person) {
+		return personRepo.save(person);
+	}
+	
+	public void deletePerson(Long id) {
+		personRepo.deleteById(id);
+	}
+	
+	public Person updatePerson(Long id, Person personDetails) {
+		Person person = personRepo.findById(id).orElseThrow();
+		person.setName(personDetails.getName());
+		person.setAge(personDetails.getAge());
+		return personRepo.save(person);
+	}
+}
